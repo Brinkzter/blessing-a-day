5df9fb7db78779449a0a341b11968eea4168dacd
Henry Brinkman <Henry.Brinkman@topicus.nl>
2018-06-12 09:03:00 +0200

    draait nu

---
+++ platforms/ios/build/blessingaday.build/Debug-iphonesimulator/blessingaday.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/blessingaday/blessingaday-Prefix.pch \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/main.m \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/TNSDebugging.h \
+  /Users/henrybrinkman/Documents/blessing-a-day/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
