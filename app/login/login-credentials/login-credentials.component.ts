626e7070328c87b7f83283e92630f47748da00f2
Henry Brinkman <Henry.Brinkman@topicus.nl>
2018-07-22 14:48:10 +0200

    test

--- app/login/login-credentials/login-credentials.component.ts
+++ app/login/login-credentials/login-credentials.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page';
 import { handleIPhoneX } from '~/utils/status-bar-utils';
+import { isIOS } from 'tns-core-modules/platform';
+import { topmost } from 'tns-core-modules/ui/frame';
+import { Color } from 'tns-core-modules/color';
 
 @Component({
     selector: 'bad-login-credentials',
@@ -13,8 +16,16 @@ export class LoginCredentialsComponent implements OnInit {
 
     public ngOnInit() {
         this.page.className = 'login-main';
+        this.page.backgroundSpanUnderStatusBar = true;
         this.page.actionBar.className = 'login-actionbar';
         this.page.actionBar.backgroundColor = '#FFF';
         handleIPhoneX(this.page);
+        if (isIOS) {
+            let red = new Color("#ff0000");
+            let navigationBar = topmost().ios.controller.navigationBar;
+            console.log(navigationBar);
+            navigationBar.translucent = true;
+            navigationBar.backgroundColor = red.ios;
+        }
     }
 }
